198122285f440b40a9c7f833b886411869005fe4
Jacob Wolf <jacob.h.wolf@gmail.com>
2021-02-12 14:37:37 +0800

    Add examples to sections

--- src/data/sections.js
+++ src/data/sections.js
@@ -44,31 +44,41 @@ export const sections = [
     speechSVGPath: "M119.921,220.673C119.921,220.673 141.464,206.977 142.446,205.995C145.398,203.044 149.953,199.806 152.948,197.202C164.651,187.026 164.492,180.18 167.386,165.707C171.695,144.165 166.302,121.378 210.254,103.11C269.157,78.627 396.799,87.869 461.67,88.172C509.035,88.393 680.15,82.098 701.59,119.218C721.339,153.412 716.669,172.034 715.304,196.147C713.675,224.934 704.01,259.162 696.852,271.092C673.721,309.643 643.788,296.455 595.445,302.059C565.329,305.55 507.124,302.872 473.327,303.545C458.812,303.834 375.79,305.17 356.418,305.903C344.572,306.351 249.926,308.548 209.353,294.712C187.291,287.189 180.74,254.7 172.859,230.318C171.664,226.62 168.975,210.802 166.468,208.294C165.768,207.595 157.734,209.976 153.643,211.533C146.719,214.168 123.333,220.673 119.921,220.673Z",
     projects: [
       {
-        projectTitle: "Making with Code",
+        projectTitle: "Blessings",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur tempor tellus, ut blandit nibh tincidunt luctus. Curabitur pretium massa ultrices felis dictum, vitae aliquet neque hendrerit. Suspendisse varius arcu eget nulla ultrices, vitae congue ipsum suscipit. Vestibulum dolor augue, cursus gravida lobortis sed, tempus ut lacus. Aliquam ligula lacus, dapibus nec mollis non, lacinia eget neque. Proin porttitor in erat non vestibulum. Morbi tristique urna volutpat, consectetur nunc sed, dictum nibh. Quisque non bibendum nibh. Vestibulum leo massa, vehicula nec leo a, dapibus iaculis quam. Sed dapibus in arcu eget tincidunt.",
         list: [
           {
-            text: "test",
-            link: "www.google.com"
+            text: "sample blessing",
+            link: "https://blessings.jacobhwolf.com/sample"
           },
           {
-            text: "test1",
-            link: "www.yahoo.com"
-          }
+            text: "github repo",
+            link: "https://github.com/wolfj95/blessings-app"
+          },
         ]
       },
       {
-        projectTitle: "Evaluating EdTech",
+        projectTitle: "Interneditor",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur tempor tellus, ut blandit nibh tincidunt luctus. Curabitur pretium massa ultrices felis dictum, vitae aliquet neque hendrerit. Suspendisse varius arcu eget nulla ultrices, vitae congue ipsum suscipit. Vestibulum dolor augue, cursus gravida lobortis sed, tempus ut lacus. Aliquam ligula lacus, dapibus nec mollis non, lacinia eget neque. Proin porttitor in erat non vestibulum. Morbi tristique urna volutpat, consectetur nunc sed, dictum nibh. Quisque non bibendum nibh. Vestibulum leo massa, vehicula nec leo a, dapibus iaculis quam. Sed dapibus in arcu eget tincidunt.",
         list: [
           {
-            text: "test",
-            link: "www.google.com"
+            text: "github repo",
+            link: "https://github.com/Tactical-Internetism/interneditor"
+          },
+        ]
+      },
+      {
+        projectTitle: "The Sky When You Were Born",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur tempor tellus, ut blandit nibh tincidunt luctus. Curabitur pretium massa ultrices felis dictum, vitae aliquet neque hendrerit. Suspendisse varius arcu eget nulla ultrices, vitae congue ipsum suscipit. Vestibulum dolor augue, cursus gravida lobortis sed, tempus ut lacus. Aliquam ligula lacus, dapibus nec mollis non, lacinia eget neque. Proin porttitor in erat non vestibulum. Morbi tristique urna volutpat, consectetur nunc sed, dictum nibh. Quisque non bibendum nibh. Vestibulum leo massa, vehicula nec leo a, dapibus iaculis quam. Sed dapibus in arcu eget tincidunt.",
+        list: [
+          {
+            text: "sample",
+            link: "https://birthday-views.glitch.me/?name=Name&date=06301997&tag=sky"
           },
           {
-            text: "test1",
-            link: "www.yahoo.com"
-          }
+            text: "glitch repo",
+            link: "https://glitch.com/edit/#!/birthday-views"
+          },
         ]
       }
     ]
@@ -80,31 +90,27 @@ export const sections = [
     speechSVGPath: "M119.921,220.673C119.921,220.673 141.464,206.977 142.446,205.995C145.398,203.044 149.953,199.806 152.948,197.202C164.651,187.026 164.492,180.18 167.386,165.707C171.695,144.165 166.302,121.378 210.254,103.11C269.157,78.627 396.799,87.869 461.67,88.172C509.035,88.393 680.15,82.098 701.59,119.218C721.339,153.412 716.669,172.034 715.304,196.147C713.675,224.934 704.01,259.162 696.852,271.092C673.721,309.643 643.788,296.455 595.445,302.059C565.329,305.55 507.124,302.872 473.327,303.545C458.812,303.834 375.79,305.17 356.418,305.903C344.572,306.351 249.926,308.548 209.353,294.712C187.291,287.189 180.74,254.7 172.859,230.318C171.664,226.62 168.975,210.802 166.468,208.294C165.768,207.595 157.734,209.976 153.643,211.533C146.719,214.168 123.333,220.673 119.921,220.673Z",
     projects: [
       {
-        projectTitle: "Making with Code",
+        projectTitle: "Constructionism 2020 Paper",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur tempor tellus, ut blandit nibh tincidunt luctus. Curabitur pretium massa ultrices felis dictum, vitae aliquet neque hendrerit. Suspendisse varius arcu eget nulla ultrices, vitae congue ipsum suscipit. Vestibulum dolor augue, cursus gravida lobortis sed, tempus ut lacus. Aliquam ligula lacus, dapibus nec mollis non, lacinia eget neque. Proin porttitor in erat non vestibulum. Morbi tristique urna volutpat, consectetur nunc sed, dictum nibh. Quisque non bibendum nibh. Vestibulum leo massa, vehicula nec leo a, dapibus iaculis quam. Sed dapibus in arcu eget tincidunt.",
         list: [
           {
-            text: "test",
+            text: "Recovering Constructionism in Computer Science: Design of a Ninth-grade Introductory Computer Science Course",
             link: "www.google.com"
           },
           {
-            text: "test1",
-            link: "www.yahoo.com"
+            text: "full proceedings",
+            link: "http://www.constructionismconf.org/wp-content/uploads/2020/05/C2020-Proceedings.pdf"
           }
         ]
       },
       {
-        projectTitle: "Evaluating EdTech",
+        projectTitle: "Honors Thesis",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur tempor tellus, ut blandit nibh tincidunt luctus. Curabitur pretium massa ultrices felis dictum, vitae aliquet neque hendrerit. Suspendisse varius arcu eget nulla ultrices, vitae congue ipsum suscipit. Vestibulum dolor augue, cursus gravida lobortis sed, tempus ut lacus. Aliquam ligula lacus, dapibus nec mollis non, lacinia eget neque. Proin porttitor in erat non vestibulum. Morbi tristique urna volutpat, consectetur nunc sed, dictum nibh. Quisque non bibendum nibh. Vestibulum leo massa, vehicula nec leo a, dapibus iaculis quam. Sed dapibus in arcu eget tincidunt.",
         list: [
           {
-            text: "test",
-            link: "www.google.com"
+            text: "Scripting Social Learning: Investigating Students’ Perceptions of Social Constructivist Learning in Minerva’s Online Learning Environment",
+            link: "https://stacks.stanford.edu/file/druid:gt160tf9972/Thesis%20Final.pdf"
           },
-          {
-            text: "test1",
-            link: "www.yahoo.com"
-          }
         ]
       }
     ]
